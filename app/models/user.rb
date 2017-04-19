b2a93bab16d11be7f43d25014cebc4abe29b3dfe
Trevor OConnor <trevoroc22@gmail.com>
2017-04-19 14:10:43 -0700

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,34 @@
+class User < ActiveRecord::Base
+  validates :user_name, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+  
+  def reset_session_token
+    self.session_token = SecureRandom::urlsafe_base64(32)
+    self.save!
+    self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(user_name, password)
+    user = self.find_by(user_name: user_name)
+    return user if user && user.is_password?(password)
+    nil
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64(32)
+  end
+end
